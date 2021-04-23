fbb2eb7a4ccd7924c26875018bc80eae5f749c71
Bryan Xu <bryanbobo123@gmail.com>
2021-04-22 20:36:58 -0700

    bx - completed api call with working chart

--- src/App.js
+++ src/App.js
@@ -3,36 +3,14 @@ import './App.css';
 import Now from './Components/Now'
 import Day from './Components/Day'
 
-function App() {
-  const [content, setContent] = useState(0);
-
-  useEffect(() => {
-    fetch('https://t1-rec-center.herokuapp.com/hello').then(res => res.json()).then(data => {
-      setContent(data.content);
-    });
-  }, []);
-
-  // var params = {
-  //   'api_key_private': 'pri_db82504af6064a37a9e1e871589105ff',
-  //   'venue_name': 'Recreation Center',
-  //   'venue_address': '516 Ocean Rd Santa Barbara, CA 93117 United States'
-  // }
-
-  // $.ajax({
-  //   "url": "https://besttime.app/api/v1/forecasts?" + new URLSearchParams(params),
-  //   "method": "POST"
-  //   }).done(function (response) {
-  //       console.log(response);
-  //   });
-    
+function App() {    
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{content}</h1>
+      {/* <header className="App-header">
         <p>Hello World!</p>
-        <Day />
-      </header>
+      </header> */}
       {/* <Now /> */}
+      <Day />
     </div>
   );
 }
--- src/Components/Day.js
+++ src/Components/Day.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 var d = new Date();
 var n = d.getDay();
+n = n-1 % 7;
+
+console.log("day number: ", n);
 
 var params = {
     'venue_id': 'ven_5965782d62435251644858524159365f51575f357263394a496843',
@@ -11,24 +14,77 @@ var params = {
     'day_int': n
 }
 
-
-export default class Now extends React.Component {
-    state = {
-        times: [],      
-    };    
-
-    
-    componentDidMount() {
-        axios.get("https://besttime.app/api/v1/forecasts/day/raw?" + new URLSearchParams(params)).then(res => {
-            console.log(res);
-            this.setState({times: res.data.analysis.day_raw});
-        })
+function rotateRight(arr, num){
+    var i;
+    for(i = 0; i < num; i++){
+        let last = arr.pop();
+        arr.unshift(last);
     }
-        
-
-    render(){
-        return <ul>{this.state.times.map(time => <li>{time}</li>)}</ul>;
-    }    
+    return arr;
 }
 
-// return <ul>{this.state.times.map(time => <li>{time}</li>)}</ul>;
+const Day = () => {
+    const [chartData, setChartData] = useState({});
+
+    const chart = () => {
+        let timeData = [];
+        let timeLabels = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
+
+        axios.get("https://besttime.app/api/v1/forecasts/day/raw?" + new URLSearchParams(params)).then(res => {
+            console.log(res)
+            for(const dataObj of res.data.analysis.day_raw){
+                timeData.push(parseInt(dataObj))
+            } 
+            rotateRight(timeLabels, 6);
+            setChartData({
+                labels: timeLabels,
+                datasets: [
+                    {
+                        label: 'Occupancy level',
+                        data: timeData,
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgb(54, 162, 235)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        });
+        console.log(timeData);
+    }
+
+    useEffect(() => {
+        chart()
+    }, [])
+    return (
+        <div>
+            <Bar data={chartData} options={{
+                responsive: true,
+                title: {text: 'Occupancy Level', display: true},
+                scales: {
+                    yAxes: 
+                        {
+                            gridLines: {
+                                show: false,
+                            },
+                            beginAtZero: true,
+                            suggestedMax: 100
+                        },
+                        xAxes: {
+                            gridLines: {
+                                show: false
+                            }
+                        }
+                }
+            }}/>
+        </div>
+    )
+}
+
+export default Day;
