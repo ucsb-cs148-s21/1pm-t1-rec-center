1bf390ca40eda48ae852b33410bcba479d9f8f83
billy <zhengwei@ucsb.edu>
2021-04-22 21:31:47 -0700

    bw - fix package-lock problems from last PR

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react';
 import AppRoute from './utils/AppRoute';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
-import Now from './Components/Now'
-import Day from './Components/Day'
+import Now from './components/Now'
+import Day from './components/Day'
 import Layout from './components/Layout';
 import Home from './view/Home';
 import About from './view/About';
 
 function App() {
   return (
+	<>
 	<BrowserRouter>
 		<Switch>
 			<AppRoute exact path="/" component={Home} layout={Layout} />
@@ -19,6 +20,7 @@ function App() {
     <div>
       <Day />
     </div>
+	</>
   );
 }
 
