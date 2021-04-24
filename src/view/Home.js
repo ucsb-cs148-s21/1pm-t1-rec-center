a880391cf0b7291505b67bca30386030f8a51eea
billy <zhengwei@ucsb.edu>
2021-04-23 18:35:41 -0700

    bw - add elements back

---
+++ src/view/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+	<>
+		<h1>Rec Cen</h1>
+		<p>A simple web app that tracks the traffic at the rec-center.</p>
+	</>
+  );
+}
+
+export default Home;
