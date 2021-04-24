67af0bccce495cc7ac8cc1fbaf683eacf554b84b
billy <zhengwei@ucsb.edu>
2021-04-23 18:15:15 -0700

    bw - fix merge conflict for navbar

---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,22 @@
+export const MenuItems =[
+    {
+        label: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        label: 'Hours',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        label: 'My Schedule',
+        url: '#',
+        cName: 'nav-links'
+    },
+   {
+        label: 'Sign up',
+        url: '#',
+        cName: 'nav-links-S'
+    }
+] //an array to keep all of the menu names so it is easier to change the contents of the menu names 
