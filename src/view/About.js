ca5474601cbdf548228392f6f90181d829ae270b
Bryan Xu <bryanbobo123@gmail.com>
2021-04-24 16:10:51 -0700

    bx - added styling and background image to homepage and aboutpage

--- src/view/About.js
+++ src/view/About.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
 
   return (
 	<>
+	<div class="aboutpage animate__animated animate__slower animate__fadeIn"> 
 		<h1>About us</h1>
-		<p></p>
+		<h6>Andy</h6>
+		<p>Hello everyone, my name is Andy. I'm a third year CS major and some of my interests include video games, cars, and being outdoors.</p>
+		<h6>Alejandro</h6>
+		<p>Hello y'all. My name is Alejandro, and I am a second year majoring in Computer Science. Aside from learning to be a better developer, my interests are in cryptocurrency, videogames, weight lifting, and movies.</p>
+		<h6>Billy</h6>
+		<p>Hello everyone, my name is Billy. I'm a third year CS major and I love coding, drawing and designing. I play a lot of sports, including soccer, badminton, and pingpong (havent done any for a while). I am also an anime fan, and speak Japanese for that sake XD.</p>
+		<h6>Bryan</h6>
+		<p>Hi! My name is Bryan. I'm a third year CS student, and my interests outside of coding include Smash, anime, and badminton. I was previously a ULA for CS16, and I'm looking forward to working together!</p>
+		<h6>Silvia</h6>
+		<p>Hi everyone, my name is Silvia Yu. I'm a third year Computer Science major. My interests outside of coding are playing escape rooms and watching anime.</p>
+	</div>
 	</>
   );
 }
