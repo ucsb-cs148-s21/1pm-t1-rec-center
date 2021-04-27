b0e569193117c95a902613d0120c1949c9b88d37
Andy Ho <andyho@ucsb.edu>
2021-04-26 23:38:57 -0700

    ah - Added some styling to the current navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import './Nav.css';
 
 const NavBar = () => {
 	return (
 		<>
 		<Navbar expand="lg">
 			<Navbar.Collapse>
-				<Nav>
-					<Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
-					<Nav.Item><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
-					<Nav.Item><Nav.Link href='/activity'>Activity</Nav.Link></Nav.Item>
-					<Nav.Item><Nav.Link href='/hours'>Hours</Nav.Link></Nav.Item>
+				<Nav className='topPart'>
+					<Nav.Item className='link'><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
+					<Nav.Item className='link'><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
+					<Nav.Item className='link'><Nav.Link href='/activity'>Activity</Nav.Link></Nav.Item>
+					<Nav.Item className='link'><Nav.Link href='/hours'>Hours</Nav.Link></Nav.Item>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
