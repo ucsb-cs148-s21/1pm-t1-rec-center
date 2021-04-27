2f49d07bfbc092032cb8fd3eeea2b2cd9aad3d96
Andy Ho <andyho@ucsb.edu>
2021-04-26 23:37:52 -0700

    ah - Fixed formatting of page to match the design of the website better

--- src/view/Activity.js
+++ src/view/Activity.js
@@ -11,14 +11,14 @@ class Activity extends Component {
     state = {  }
     render() { 
         return (
-            <div className='App-header'>
-                <h1 className="text-center mb-5 font-weight-bold">Recreation Center Activity</h1>
+            <div className='App-header animate__animated animate__fast animate__fadeIn'>
+                <h1 className="text-center mb-5 font-weight-bold title">Recreation Center Activity</h1>
                 <div className="w-75 h-75">
                 <CardGroup style={{ display: 'flex', flexDirection: 'row'}}>
-                    <Card border='dark' text='white' bg="dark">
-                    <Card.Title className="text-center mt-4 mb-1 font-weight-bold">Hourly Forecast</Card.Title>
+                    <Card className='card' text='black' bg="rgba(240, 240, 240, 0.9)">
+                    <Card.Title><h3 className="text-center mt-4 mb-1 font-weight-bold">Hourly Forecast</h3></Card.Title>
                         <Card.Body>
-                            <Now />
+                            <h6 className='occupancy'><Now /></h6>
                             <Day />
                         </Card.Body>
                     </Card>
