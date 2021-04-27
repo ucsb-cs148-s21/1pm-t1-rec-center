31d958fa081986922cbfe584d415e3ebfeb6f4bd
XuperBryan <bryanbobo123@gmail.com>
2021-04-27 16:29:23 -0700

    bx - update api key

--- src/components/Now.js
+++ src/components/Now.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 var params = {
     'venue_id': 'ven_5965782d62435251644858524159365f51575f357263394a496843',
-    'api_key_public': 'pub_e7764657ade1430cb9990b81dd156525'
+    'api_key_public': 'pub_9a1b3395b4ba4b42973adb9b6bb0f646'
 }
 
 export default class Now extends React.Component {
