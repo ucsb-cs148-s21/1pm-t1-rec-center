10f0a00edfef69b3e3150d40101d04354182c73b
Andy Ho <andyho@ucsb.edu>
2021-04-27 20:02:31 -0700

    ah - Adjusted some coloring and fixed layout for activity page

--- src/view/Activity.js
+++ src/view/Activity.js
@@ -12,18 +12,14 @@ class Activity extends Component {
     render() { 
         return (
             <div className='App-header animate__animated animate__fast animate__fadeIn'>
-                <h1 className="text-center mb-5 font-weight-bold title">Recreation Center Activity</h1>
-                <div className="w-75 h-75">
-                <CardGroup style={{ display: 'flex', flexDirection: 'row'}}>
+                <h1>Recreation Center Activity</h1>
                     <Card className='card' text='black' bg="rgba(240, 240, 240, 0.9)">
-                    <Card.Title><h3 className="text-center mt-4 mb-1 font-weight-bold">Hourly Forecast</h3></Card.Title>
+                    <Card.Title><h3>Hourly Forecast</h3></Card.Title>
                         <Card.Body>
                             <h6 className='occupancy'><Now /></h6>
                             <Day />
                         </Card.Body>
                     </Card>
-                </CardGroup>
-                </div>
             </div>
         );
     }
