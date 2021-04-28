6cf58c45adb529dbe26c54f98a3cf06833b440aa
Andy Ho <andyho@ucsb.edu>
2021-04-27 20:03:30 -0700

    ah - Removed collapse to keep the navbar from disappering when resizing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,12 @@ const NavBar = () => {
 	return (
 		<>
 		<Navbar expand="lg">
-			<Navbar.Collapse>
 				<Nav className='topPart'>
 					<Nav.Item className='link'><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
 					<Nav.Item className='link'><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
 					<Nav.Item className='link'><Nav.Link href='/activity'>Activity</Nav.Link></Nav.Item>
 					<Nav.Item className='link'><Nav.Link href='/hours'>Hours</Nav.Link></Nav.Item>
 				</Nav>
-			</Navbar.Collapse>
 		</Navbar>
 		</>
 	);
