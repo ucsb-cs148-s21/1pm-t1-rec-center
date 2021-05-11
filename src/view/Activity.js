108cd61ee572368e84faa154d1541b6b5553bce2
Bryan Xu <bryanbobo123@gmail.com>
2021-05-11 00:19:10 -0700

    bx - added functionality where current time is in red, and removed now.js, as it has been integrated into activity.js

--- src/view/Activity.js
+++ src/view/Activity.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Now from './../components/Now';
 import Day from './../components/Day';
 import { Card } from 'react-bootstrap';
-import { CardGroup } from 'react-bootstrap';
+// import { CardGroup } from 'react-bootstrap';
 import './Activity.css'
 
 
@@ -10,13 +9,14 @@ import './Activity.css'
 class Activity extends Component {
     state = {  }
     render() { 
+        let currentDay = new Date();
+        currentDay = currentDay.toDateString();
         return (
             <div className='App-header animate__animated animate__fast animate__fadeIn'>
                 <h1>Recreation Center Activity</h1>
                     <Card className='card' text='black' bg="rgba(240, 240, 240, 0.9)">
-                    <Card.Title><h3>Hourly Forecast</h3></Card.Title>
+                    <Card.Title><h3>Forecast for {currentDay}</h3></Card.Title>
                         <Card.Body>
-                            <h6 className='occupancy'><Now /></h6>
                             <Day />
                         </Card.Body>
                     </Card>
