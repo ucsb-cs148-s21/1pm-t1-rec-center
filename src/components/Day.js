9620e9a1f81eccd52ce4b71361dd9dbceed9c658
Andy Ho <andyho@ucsb.edu>
2021-05-22 23:39:59 -0700

    ah - Fixed day component to rerender properly and made activity page render dashboard

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,44 +3,40 @@ import { Bar } from 'react-chartjs-2';
 
 
 let timeLabels = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am', '1am', '2am', '3am', '4am', '5am'];
-
-
 export default class Day extends Component {
-    state = {
-        chartData: {
-            labels: timeLabels,
-            datasets: [
-                {
-                    label: 'Activity',
-                    data: this.props.day.data,
-                    backgroundColor: this.props.day.colors,
-                    borderColor: this.props.day.colorBorders,
-                    borderWidth: 1
-                }
-            ]
-        }
-    }
 
-    render() {
-        console.log(this.state.chartData); 
+    render() { 
         return (  
-            <Bar data={this.state.chartData} options={{
-                responsive: true,
-                title: {text: 'Occupancy Level', display: true},
-                scales: {
-                    yAxes: 
+            <Bar data={
+                {
+                    labels: timeLabels,
+                    datasets: [
                         {
-                            gridLines: {
-                                show: false,
-                            },
-                            beginAtZero: true,
-                            suggestedMax: 100
-                        },
-                        xAxes: {
-                            gridLines: {
-                                show: false
-                            }
+                            label: 'Activity',
+                            data: this.props.day.data,
+                            backgroundColor: this.props.day.colors,
+                            borderColor: this.props.day.colorBorders,
+                            borderWidth: 1
                         }
+                    ]
+                }} 
+                options={{
+                    responsive: true,
+                    title: {text: 'Occupancy Level', display: true},
+                    scales: {
+                        yAxes: 
+                            {
+                                gridLines: {
+                                    show: false,
+                                },
+                                beginAtZero: true,
+                                suggestedMax: 100
+                            },
+                            xAxes: {
+                                gridLines: {
+                                    show: false
+                                }
+                            }
                 }
             }}/>
         );
--- src/view/Activity.js
+++ src/view/Activity.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Day from './../components/Day';
-import { Card } from 'react-bootstrap';
-// import { CardGroup } from 'react-bootstrap';
+import Dashboard from '../components/dashboard';
 import './Activity.css'
 
 
@@ -14,12 +12,7 @@ class Activity extends Component {
         return (
             <div className='App-header animate__animated animate__fast animate__fadeIn'>
                 <h1>Recreation Center Activity</h1>
-                    <Card className='card' text='black' bg="rgba(240, 240, 240, 0.9)">
-                    <Card.Title><h3>Forecast for {currentDay}</h3></Card.Title>
-                        <Card.Body>
-                            <Day />
-                        </Card.Body>
-                    </Card>
+                    <Dashboard/>
             </div>
         );
     }
