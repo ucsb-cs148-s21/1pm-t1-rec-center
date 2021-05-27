4a4f16cfec63b079acc6485031e99f71ec48e4eb
Bryan Xu <bryanbobo123@gmail.com>
2021-05-27 16:44:22 -0700

    bx - added current temp

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,11 +10,16 @@ let weather = "sunny";
 let icon = "01d";
 let urlFirst = "http://openweathermap.org/img/wn/";
 let urlSecond = "@2x.png";
+let temperature = 0;
 
 axios.get("https://api.openweathermap.org/data/2.5/weather?" + new URLSearchParams(params)).then(res => {
         weather = res.data.weather[0].description;
         icon = res.data.weather[0].icon;
         console.log("weather data: ", res.data.weather[0]);
+        temperature = res.data.main.temp;
+        console.log("recorded temp: ",temperature);
+        temperature = Math.round(((temperature-273.15)*1.8)+32);
+        console.log("converted temp: ",temperature);
     })
     .catch(err => {
         console.log(err)
@@ -29,6 +34,7 @@ class Weather extends Component {
                     src={urlFirst + icon + urlSecond} 
                     alt="weather icon" 
                 />
+                Temperature: {temperature}&deg; F
             </div>
         )
     }
