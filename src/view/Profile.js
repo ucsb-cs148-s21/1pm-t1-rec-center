68181c3d760bc69a5c2214335b720db430ec5a8b
billy <zhengwei@ucsb.edu>
2021-06-01 23:02:11 -0700

    bw - added calendar ui api, needs refinement

--- src/view/Profile.js
+++ src/view/Profile.js
@@ -1,8 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import getUser from '../utils/get-user';
+import { format, subHours, startOfMonth } from 'date-fns';
+import {
+  MonthlyBody,
+  MonthlyDay,
+  MonthlyCalendar,
+  MonthlyNav,
+  DefaultMonthlyEventItem,
+} from '@zach.codes/react-calendar';
+import '@zach.codes/react-calendar/dist/calendar-tailwind.css';
 
 const Profile = () => {
   const user = getUser();
+
+  let [currentMonth, setCurrentMonth] = useState(new Date());
 
   function appendPre(message) {
 	var pre = document.getElementById('content');
@@ -10,8 +21,17 @@ const Profile = () => {
 	pre.appendChild(textContent);
   }
   
+  var API_KEY = process.env.REACT_APP_API_KEY;
+  var events = [];
+  
   const listUpcomingEvents = () => {
-	console.log(window.gapi.client);
+	console.log(window.gapi.client.calendar.events.list);
+	window.gapi.client.calendar.calendarList.list({
+		  
+		}).then(function(response) {
+		  var calendars = response.result.items;
+		  console.log(calendars);
+		});
 	window.gapi.client.load('calendar', 'v3', () => {
 		window.gapi.client.calendar.events.list({
 		  'calendarId': 'primary',
@@ -23,7 +43,7 @@ const Profile = () => {
 		}).then(function(response) {
 		  var events = response.result.items;
 		  console.log(events);
-		  appendPre('Upcoming events:');
+		  //appendPre('Upcoming events:');
 
 		  if (events.length > 0) {
 			for (var i = 0; i < events.length; i++) {
@@ -32,22 +52,44 @@ const Profile = () => {
 			  if (!when) {
 				when = event.start.date;
 			  }
-			  appendPre(event.summary + ' (' + when + ')')
+			  //appendPre(event.summary + ' (' + when + ')')
+			  events[i].title = event.summary;
+			  events[i].date = when;
 			}
 		  } else {
-			appendPre('No upcoming events found.');
+			//appendPre('No upcoming events found.');
 		  }
+		  console.log(events);
 		});
 	});
   };
 
   return (
-	<div className="profile" data-testid="profile"> 
-		<h1>This is your profile page</h1>
-		<pre>{JSON.stringify(user, null, "\t")}</pre>
-		<button onClick={listUpcomingEvents}>show calendar</button>
-		<pre id="content"></pre>
-	</div>
+	  <>
+		<div className="profile" data-testid="profile"> 
+			<h1>Calendar</h1>
+			<button onClick={listUpcomingEvents}>update calendar</button>
+			<pre id="content"></pre>
+		</div>
+		<MonthlyCalendar currentMonth={currentMonth} 
+		  onCurrentMonthChange={date => setCurrentMonth(date)}>
+		  <MonthlyNav />
+		  <MonthlyBody events={events}>
+		    <MonthlyDay
+			  renderDay={data =>
+			    data.map((item, index) => (
+			      <DefaultMonthlyEventItem
+				    key={index}
+				    title={item.title}
+				    // Format the date here to be in the format you prefer
+				    date={format(item.date, 'k:mm')}
+				  />
+			    ))
+			  }
+		    />
+		  </MonthlyBody>
+	    </MonthlyCalendar>
+	  </>
   );
 }
 
