a856992c204d3603f319b49cb0c68bc9554382f2
XuperBryan <bryanbobo123@gmail.com>
2021-06-04 18:26:06 -0700

    bx - fixed typo

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import { Card, Tab, Tabs, Nav } from 'react-bootstrap';
+import { Tab, Tabs, Nav } from 'react-bootstrap';
 
 
 const Home = () => {
@@ -8,27 +8,9 @@ const Home = () => {
 	<>
 	<div className="homepage" data-testid="welcome">
 		<h1 className="animate__animated animate__backInDown">WELCOME</h1>
-		<h2 className="animate__animated animate__backInDown">to the USCB Recreation Center Activity Tracker</h2>
+		<h2 className="animate__animated animate__backInDown">to the UCSB Recreation Center Activity Tracker</h2>
 		<Nav.Link href='/activity'><button className="animate__animated animate__backInDown">Track Activity</button></Nav.Link>
 	</div>
-	<Card>
-		<Tabs>
-			<Tab eventKey='Monday' profile='Monday'>
-			</Tab>
-			<Tab eventKey='Tuesday' profile='Tuesday'>
-			</Tab>
-			<Tab eventKey='Wednesday' profile='Wednesday'>
-			</Tab>
-			<Tab eventKey='Thursday' profile='Thursday'>
-			</Tab>
-			<Tab eventKey='Friday' profile='Friday'>
-			</Tab>
-			<Tab eventKey='Saturday' profile='Saturday'>
-			</Tab>
-			<Tab eventKey='Sunday' profile='Sunday'>
-			</Tab>
-		</Tabs>
-	</Card>
 	</>
   );
 }
